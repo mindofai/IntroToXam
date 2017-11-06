58298b6b2ae5bccdc27f8b704bff824c3c9d9677
AMAIISDOM\bgarcia <BryanAnthony.Garcia@navitaire.com>
2017-11-06 16:31:21 +0800

    Base ui

--- IntroToXam/IntroToXam/MainPage.xaml.cs
+++ IntroToXam/IntroToXam/MainPage.xaml.cs
@@ -13,5 +13,10 @@ namespace IntroToXam
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
